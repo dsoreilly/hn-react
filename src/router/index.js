316375d8b1340555591900fe5bee55d25ab567e6
David O'Reilly <hello@dsoreilly.me>
2022-12-01 16:03:44 +0000

    Set the basename

--- src/router/index.js
+++ src/router/index.js
@@ -8,45 +8,48 @@ import ItemsView from '../views/Items';
 import RootView from '../views/Root';
 import UserView from '../views/User';
 
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootView />,
-        errorElement: <ErrorView />,
-        children: [
-            {
-                errorElement: <ErrorView />,
-                children: [
-                    {
-                        index: true,
-                        element: <ItemsView />,
-                    },
-                    {
-                        path: 'item/:id',
-                        element: <ItemView />,
-                    },
-                    {
-                        path: '/ask',
-                        element: <ItemsView type="ask" />,
-                    },
-                    {
-                        path: '/jobs',
-                        element: <ItemsView type="job" />,
-                    },
-                    {
-                        path: '/newest',
-                        element: <ItemsView type="new" />,
-                    },
-                    {
-                        path: '/show',
-                        element: <ItemsView type="show" />,
-                    },
-                    {
-                        path: 'user/:id',
-                        element: <UserView />,
-                    },
-                ],
-            },
-        ],
-    },
-]);
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <RootView />,
+            errorElement: <ErrorView />,
+            children: [
+                {
+                    errorElement: <ErrorView />,
+                    children: [
+                        {
+                            index: true,
+                            element: <ItemsView />,
+                        },
+                        {
+                            path: 'item/:id',
+                            element: <ItemView />,
+                        },
+                        {
+                            path: '/ask',
+                            element: <ItemsView type="ask" />,
+                        },
+                        {
+                            path: '/jobs',
+                            element: <ItemsView type="job" />,
+                        },
+                        {
+                            path: '/newest',
+                            element: <ItemsView type="new" />,
+                        },
+                        {
+                            path: '/show',
+                            element: <ItemsView type="show" />,
+                        },
+                        {
+                            path: 'user/:id',
+                            element: <UserView />,
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    { basename: '/hn-react/' }
+);
