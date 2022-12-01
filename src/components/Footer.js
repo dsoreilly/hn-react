2f6eec7061a02db6caa0e03b7b0a7157f0bfa8f2
David O'Reilly <hello@dsoreilly.me>
2022-12-01 15:34:43 +0000

    Update footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,24 @@ function Header() {
         <footer className="container mx-auto my-4 flex flex-row justify-center">
             <div className="text-center text-xs text-slate-500">
                 React-based{' '}
-                <a className="hover:underline" href="">
+                <a
+                    className="hover:underline"
+                    href="https://news.ycombinator.com"
+                >
                     Hacker News
                 </a>{' '}
                 clone using the{' '}
-                <a className="hover:underline" href="">
+                <a
+                    className="hover:underline"
+                    href="https://github.com/HackerNews/API"
+                >
                     official Firebase API &#9883;
                 </a>
                 <br />
-                <a className="hover:underline" href="">
+                <a
+                    className="hover:underline"
+                    href="https://github.com/dsoreilly/hn-react"
+                >
                     View the source on GitHub
                 </a>
             </div>
