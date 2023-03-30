05d04c6535c6d7013a2b3b8e11cf6b09da152135
David O'Reilly <hello@dsoreilly.me>
2023-03-30 13:43:04 +0100

    update navbar styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ import Navbar from "./Navbar.jsx";
 function Header() {
   return (
     <header className="sticky top-0 w-full border-slate-800 bg-orange-500/90 shadow backdrop-blur dark:border-b dark:bg-slate-900/90">
-      <div className="container mx-auto flex flex-row p-6">
-        <h1 className="font-semibold">
+      <div className="container mx-auto flex flex-row px-6">
+        <h1 className="py-6 font-semibold">
           <Link
             className="text-slate-100 hover:text-orange-300 dark:text-orange-500"
             to="/"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,23 +7,51 @@ function Navbar() {
   return (
     <nav>
       <ul className="mx-2 flex flex-row">
-        <li className="mx-2 text-slate-100 dark:text-orange-500">
-          <NavLink className="hover:text-orange-300" to="/newest">
+        <li className="mx-2">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "block border-b-2 border-slate-100 py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+                : "block border-b-2 border-transparent py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+            }
+            to="/newest"
+          >
             New
           </NavLink>
         </li>
-        <li className="mx-2 text-slate-100 dark:text-orange-500">
-          <NavLink className="hover:text-orange-300" to="/ask">
+        <li className="mx-2">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "block border-b-2 border-slate-100 py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+                : "block border-b-2 border-transparent py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+            }
+            to="/ask"
+          >
             Ask
           </NavLink>
         </li>
-        <li className="mx-2 text-slate-100 dark:text-orange-500">
-          <NavLink className="hover:text-orange-300" to="/show">
+        <li className="mx-2 ">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "block border-b-2 border-slate-100 py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+                : "block border-b-2 border-transparent py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+            }
+            to="/show"
+          >
             Show
           </NavLink>
         </li>
-        <li className="mx-2 text-slate-100 dark:text-orange-500">
-          <NavLink className="hover:text-orange-300" to="/jobs">
+        <li className="mx-2">
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "block border-b-2 border-slate-100 py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+                : "block border-b-2 border-transparent py-6 text-slate-100 hover:text-orange-300 dark:text-orange-500"
+            }
+            to="/jobs"
+          >
             Jobs
           </NavLink>
         </li>
