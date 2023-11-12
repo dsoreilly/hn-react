c7c9359bbafe0e8f7c9c69873b2664839ef80801
David O'Reilly <hello@dsoreilly.me>
2023-11-12 19:01:57 +0000

    update footer comp text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="container mx-auto my-4 flex flex-col justify-center px-6 text-center text-xs text-slate-500">
       <p className="mb-2">
-        React-based{" "}
+        React based{" "}
         <a
           className="whitespace-nowrap hover:underline"
           href="https://news.ycombinator.com"
