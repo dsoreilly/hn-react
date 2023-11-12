57c0ff66390849a0f1c37eb37af78260d50c0ac5
David O'Reilly <hello@dsoreilly.me>
2023-11-12 20:30:09 +0000

    type checking

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-// @ts-nocheck
-
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./components/App";
 import store from "./store";
 import "./main.css";
 
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
